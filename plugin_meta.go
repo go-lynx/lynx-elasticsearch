f70534e75c965e1ea1e1c02e066c42d504d83ef4
tan-zhuo <tanzhuo@aliyun.com>
2026-03-02 21:52:34 +0900

    chore: bump pluginVersion to v1.5.4

--- plugin_meta.go
+++ plugin_meta.go
@@ -9,7 +9,7 @@ const (
 	// Plugin unique name
 	pluginName = "elasticsearch.client"
 	// Plugin version number
-	pluginVersion = "v1.0.0"
+	pluginVersion = "v1.5.4"
 	// Plugin description
 	pluginDescription = "elasticsearch plugin for lynx framework"
 	// Configuration prefix, used to read plugin-related configuration from config
